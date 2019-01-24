298c154f09789cfab8a7f0ed41f6beee1048c115
Nick <beznosn@gmail.com>
2019-01-24 11:53:57 +0200

    Initial Commit

---
+++ Set - the card game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set - the card game
+//
+//  Created by Nick Beznos on 1/24/19.
+//  Copyright © 2019 Nick Beznos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
