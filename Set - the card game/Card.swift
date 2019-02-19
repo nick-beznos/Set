013abd29da56dc7a2f504ca73c6881a7ac51482c
Nick <beznosn@gmail.com>
2019-02-19 12:20:24 +0200

    Card enums added

--- Set - the card game/Card.swift
+++ Set - the card game/Card.swift
@@ -8,37 +8,69 @@
 
 import Foundation
 
-struct Card: Hashable {
-    
-    var hashValue: Int { return identifier
-    }
-    static func ==(lhs: Card, rhs: Card) -> Bool {
-        return lhs.identifier == rhs.identifier
-    }
-    
-    var identifier: Int
+struct Card {
+   
 
     var color = 0
     var shape = 0
     var shading = 0
     var number = 0
     var isSelectedCard = false
-
-    private static var identifierFactory = 0
     
-    private static func getUniqueIdentifier() -> Int{
-        identifierFactory += 1
-        return identifierFactory
+    enum Color {
+        case green
+        case purple
+        case red
+        
+        static var all : [Color] {
+           return [.green, .purple, .red]
+        }
     }
-    
-    
-    init (color: Int, shape: Int, shading: Int, number: Int) {
-        self.color = color
-        self.shape = shape
-        self.shading = shading
-        self.number = number
-        self.identifier = Card.getUniqueIdentifier()
+    enum Shape: String {
+        case triangle = "▲"
+        case circle = "●"
+        case rectangle = "■"
+        
+        static var all : [Shape] {
+            return [.triangle, .circle, .rectangle]
+        }
+    }
+    enum Shading {
+        case full
+        case partial
+        case circle
+        
+        static var all : [Shading] {
+            return [.full, .partial, .circle]
+        }
+    }
+    enum Number: Int{
+        case one = 1
+        case two = 2
+        case three = 3
+        
+        static var all : [Number] {
+            return [.one, .two, .three]
+        }
 
     }
 
+    
+//    init (color: Int, shape: Int, shading: Int, number: Int) {
+//        self.color = color
+//        self.shape = shape
+//        self.shading = shading
+//        self.number = number
+//    }
+}
+
+extension Card : Equatable {
+    static func ==(lhs: Card, rhs: Card) -> Bool {
+        return
+        (lhs.color == rhs.color) &&
+        (lhs.shape == rhs.shape) &&
+        (lhs.shading == rhs.shading) &&
+        (lhs.number == rhs.number)
+        
+    }
 }
---
+++ Set - the card game/CardDeck.swift
@@ -0,0 +1,9 @@
+//
+//  CardDeck.swift
+//  Set - the card game
+//
+//  Created by Nick Beznos on 2/18/19.
+//  Copyright © 2019 Nick Beznos. All rights reserved.
+//
+
+import Foundation
